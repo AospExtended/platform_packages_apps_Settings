2b5f6d2442cc3908c84f53e7729988d65cac1e1c
Samuel Asteberg <samuel.asteberg@sonymobile.com>
2014-11-26 16:13:35 +0100

    Wi-Fi tethering switch was briefly in wrong state
    If Wi-Fi tethering is on when entering the "Tethering &
    portable hotspot" activity, the switch for Wi-Fi tethering
    is briefly displayed as off and then goes to on. The icon in
    status bar indicates that Wi-Fi tethering is on during this
    behaviour, so it is only the switch that is not correctly set
    when the activity is started. To fix this, the state of Wi-Fi
    tethering is read from the WifiManager when the activity is
    created.
    
    Change-Id: Icec7a1465652976dd1334850a3d5649f52243ce2

--- src/com/android/settings/TetherSettings.java
+++ src/com/android/settings/TetherSettings.java
@@ -302,6 +302,8 @@ public class TetherSettings extends RestrictedSettingsFragment
                     mWifiConfig.SSID,
                     mSecurityType[index]));
         }
+        // Init the switch to current state
+        mEnableWifiAp.setChecked(mWifiManager.isWifiApEnabled());
     }
 
     @Override
