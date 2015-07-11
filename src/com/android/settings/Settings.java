32dd56b4da6bb457e98cbff7c05767e050683537
Meticulus <theonejohnnyd@gmail.com>
2015-07-11 21:09:22 +0200

    Settings: Live Volume Steps [2/3]
    Change-Id: I1cf0d9e5933a846600a723cb7720d47d3bd2d22a
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -251,4 +251,5 @@ public class Settings extends SettingsActivity {
     // LiveDisplay
     public static class LiveDisplaySettingsActivity extends SettingsActivity { /* empty */ }
 
+    public static class VolumeStepsFragmentActivity extends SettingsActivity { /* empty */ }
 }
