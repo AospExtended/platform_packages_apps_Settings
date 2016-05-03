d1b02726392ae7d132205d6c12f58afb2ebd07da
Ankit Gupta <agupta@cyngn.com>
2016-05-03 13:41:41 -0700

    Settings: sync qs invert color with color inversion in Settings
    Color inversion in settings is not consistent with the quick
    settings tiles for the inverted color.
    This patch registers the invert color settings variable to
    content resolver to apply any change to it from either
    qs tiles or settings menu.
    
    Change-Id: Ib1433238d4985e930f3a1c650198dbeb05ccba52
    Issue-id: FEIJ-164

--- src/com/android/settings/accessibility/AccessibilitySettings.java
+++ src/com/android/settings/accessibility/AccessibilitySettings.java
@@ -158,7 +158,7 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
             new SettingsContentObserver(mHandler) {
                 @Override
                 public void onChange(boolean selfChange, Uri uri) {
-                    updateServicesPreferences();
+                    updateAllPreferences();
                 }
             };
 
