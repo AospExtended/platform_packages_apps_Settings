564bab7d22f3727db20f3ecabf721598e1d47a77
srisurya95 <srisurya95@gmail.com>
2016-07-29 02:44:25 -0400

    Update developer info

--- src/com/android/settings/aosprro/devs.java
+++ src/com/android/settings/aosprro/devs.java
@@ -21,7 +21,7 @@ import android.os.Bundle;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.R;
 
-import com.android.internal.logging.MetricsLogger;
+import com.android.internal.logging.MetricsProto.MetricsEvent;
 
 public class devs extends SettingsPreferenceFragment {
 
@@ -33,7 +33,7 @@ public class devs extends SettingsPreferenceFragment {
 
     @Override
     protected int getMetricsCategory() {
-        return MetricsLogger.DONT_TRACK_ME_BRO;
+	return MetricsEvent.DEVINFO;
     }
 
 }
