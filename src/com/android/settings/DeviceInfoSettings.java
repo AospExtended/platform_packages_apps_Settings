3ca73236c79df7bacb7a73b5f126598a273051b1
Chris Soyars <ctso@ctso.me>
2016-10-01 18:18:27 +0000

    Add Extended version and build date to Settings
    Change-Id: I596064a80210517ff0dae30401f71b52adabd614
    
    Rename modversion
    
    Change-Id: I6557d5aa68a3ad00166cdd2b8951f425ad7800df
    
    DeviceInfoSettings: Use ro.cm.display.version instead of ro.cm.version
    
    Follows the same logic as ro.build.display.id/ro.build.id:
    engineering, debug and unsigned builds will have no distinction,
    user and signed builds will show a simplified version
    
    Change-Id: I713ccb2445ea2e333e831129ef7733fd518282eb
    
    settings: This is CyanogenMod!
    
    Change-Id: If9f14d6579b18c4ae049d35e312dd2d98746aced
    
    Change-Id: I6557d5aa68a3ad00166cdd2b8951f425ad7800df
    
    Settings: Use cyanogenmod.os.Build for props.
    
    Change-Id: I7c1ac67a001c090e784ec2b02cefdeb1026572b5
    
    Settings: Add CM Api level to about phone.
    
    Change-Id: I802202719ca9a2d1f98fda807f133efceec7be4a
    
    Settings: Update strings for api level.
    
    Change-Id: I9ed5a1020ad5ee750c9270897710784e31ce354f
    
    Added build date (ro.build.date from build.prop) to device info.
    
    Change-Id: I5e19ee04de5c26822e171d4af8f4bfc052be9b74
    
    CM PlatLogo: CyanogenMod version preference (2/2)
    
    Binds the CyanogenMod version preference to launch CM easter egg
    upon multiple taps.
    
    Change-Id: I8e06642944d24581c0932c9964f2d9615bfcb832
    
    Settings: Use cm build properties correctly.
    
    TICKET: CYNGNOS-2379
    Change-Id: I8342589c52d633c044570be02f48c7f5a48f788c
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>
     Changes to be committed:
    	new file:   res/values/ex_strings.xml
    	modified:   res/xml/device_info_settings.xml
    	modified:   src/com/android/settings/DeviceInfoSettings.java
    
     Author:    Chris Soyars <ctso@ctso.me>
     Date:      Sat Oct 1 18:18:27 2016 +0000
    
    Conflicts:
    	src/com/android/settings/DeviceInfoSettings.java

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -74,6 +74,9 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String KEY_QGP_VERSION = "qgp_version";
     private static final String PROPERTY_QGP_VERSION = "persist.qgp.version";
 
+    private static final String KEY_EXTENDED_VERSION = "extended_version";
+    private static final String KEY_MOD_BUILD_DATE = "build_date";
+
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
     long[] mHits = new long[3];
@@ -129,6 +132,10 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setValueSummary(KEY_MBN_VERSION, PROPERTY_MBN_VERSION);
         removePreferenceIfPropertyMissing(getPreferenceScreen(), KEY_MBN_VERSION,
                 PROPERTY_MBN_VERSION);
+        findPreference(KEY_EXTENDED_VERSION).setSummary(
+                android.os.Build.EXTENDED_DISPLAY_VERSION);
+        findPreference(KEY_EXTENDED_VERSION).setEnabled(true);
+        setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
@@ -283,6 +290,21 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
             }
         } else if (preference.getKey().equals(KEY_DEVICE_FEEDBACK)) {
             sendFeedback();
+
+        } else if (preference.getKey().equals(KEY_EXTENDED_VERSION)) {
+            System.arraycopy(mHits, 1, mHits, 0, mHits.length-1);
+            mHits[mHits.length-1] = SystemClock.uptimeMillis();
+            if (mHits[0] >= (SystemClock.uptimeMillis()-500)) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.putExtra("is_extended", true);
+                intent.setClassName("android",
+                        com.android.internal.app.PlatLogoActivity.class.getName());
+                try {
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.e(LOG_TAG, "Unable to start activity " + intent.toString());
+                }
+            }
         }
         return super.onPreferenceTreeClick(preference);
     }
@@ -333,6 +355,14 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
             findPreference(preference).setSummary(
                     SystemProperties.get(property,
                             getResources().getString(R.string.device_info_default)));
+        } catch (RuntimeException e) {
+            // No recovery
+        }
+    }
+
+    private void setExplicitValueSummary(String preference, String value) {
+        try {
+            findPreference(preference).setSummary(value);
         } catch (RuntimeException e) {
             // No recovery
         }
