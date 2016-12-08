855014c3ff50dbded6956f36da67d94eb2a4c1c5
maxwen <max.weninger@gmail.com>
2016-12-09 00:30:24 +0100

    Use same way to display free storage in all places
    Change-Id: I15687295d6a6bc134fa5c14dc95a5d31350980ec

--- src/com/android/settings/deviceinfo/StorageSettings.java
+++ src/com/android/settings/deviceinfo/StorageSettings.java
@@ -167,8 +167,8 @@ public class StorageSettings extends SettingsPreferenceFragment implements Index
         mInternalCategory.addPreference(mInternalSummary);
 
         int privateCount = 0;
-        long privateUsedBytes = 0;
         long privateTotalBytes = 0;
+        long privateFreeBytes = 0;
 
         final List<VolumeInfo> volumes = mStorageManager.getVolumes();
         Collections.sort(volumes, VolumeInfo.getDescriptionComparator());
@@ -182,8 +182,8 @@ public class StorageSettings extends SettingsPreferenceFragment implements Index
                         new StorageVolumePreference(context, vol, color, volumeTotalBytes));
                 if (vol.isMountedReadable()) {
                     final File path = vol.getPath();
-                    privateUsedBytes += (volumeTotalBytes - path.getFreeSpace());
                     privateTotalBytes += volumeTotalBytes;
+                    privateFreeBytes += path.getFreeSpace();
                 }
             } else if (vol.getType() == VolumeInfo.TYPE_PUBLIC) {
                 StorageVolumePreference ExStorageVolumePreference =
@@ -195,6 +195,7 @@ public class StorageSettings extends SettingsPreferenceFragment implements Index
                 mExternalCategory.addPreference(ExStorageVolumePreference);
             }
         }
+        long privateUsedBytes = privateTotalBytes - privateFreeBytes;
 
         // Show missing private volumes
         final List<VolumeRecord> recs = mStorageManager.getVolumeRecords();
