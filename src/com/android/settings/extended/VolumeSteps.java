712631c099e78f3efac580074b2c2267410d7595
ishubhamsingh <coolsks94@gmail.com>
2016-12-17 00:35:13 +0000

    Fix derps
    Conflicts:
    	src/com/android/settings/DevelopmentSettings.java

--- src/com/android/settings/extended/VolumeSteps.java
+++ src/com/android/settings/extended/VolumeSteps.java
@@ -139,7 +139,7 @@ public class VolumeSteps extends SettingsPreferenceFragment implements
 
     @Override
     protected int getMetricsCategory() {
-        return MetricsEvent.DIRTYTWEAKS;
+        return MetricsEvent.EXTENSIONS;
     }
 
     private SharedPreferences getDefaultStepsPrefs() {
