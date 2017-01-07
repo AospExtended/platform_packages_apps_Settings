826a200406c130157838b55336000bb2bf2633ca
Sagar Shah <sagarshah2326@gmail.com>
2017-01-07 21:17:24 +0000

    Settings: About AospExtended Bringup Credits: akhilnarang KreAch3R Adarsh-MR
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -76,7 +76,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String PROPERTY_MBN_VERSION = "persist.mbn.version";
     private static final String KEY_QGP_VERSION = "qgp_version";
     private static final String PROPERTY_QGP_VERSION = "persist.qgp.version";
-
+    private static final String KEY_AEX_ABOUT_INFO = "aex_about_info";
     private static final String KEY_EXTENDED_VERSION = "extended_version";
     private static final String KEY_MOD_BUILD_DATE = "build_date";
     private static final String KEY_VENDOR_VERSION = "vendor_version";
