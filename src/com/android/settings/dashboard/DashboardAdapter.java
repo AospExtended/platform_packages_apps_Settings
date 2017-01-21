f0aafe7e8135037009cfe1a90f0a48d984fb98a9
Alex Cruz <mazdarider23@gmail.com>
2017-01-21 10:21:19 +0100

    Dashboard summaries double lines toggle [2/2]
    Inspired by @xWASABI and his commit here
    TeamTwisted/packages_apps_Settings@3c62ed2
    
    This would come in handy for tablets that use 2 rows in the dashboard and for when
    translations are just too big to fit in 1 line or bigger DPI
    
    - Removed background color on the main dashboard, looks better when applying
    2/3 columns and does not break any themes so win win
    
    Change-Id: I5cf36cb619bbfa0e1e26fc190957dfb595854f86

--- src/com/android/settings/dashboard/DashboardAdapter.java
+++ src/com/android/settings/dashboard/DashboardAdapter.java
@@ -292,7 +292,7 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
 
     private void showRemoveOption(View v, final Tile suggestion) {
         PopupMenu popup = new PopupMenu(
-                new ContextThemeWrapper(mContext, R.style.Theme_AppCompat_DayNight), v);
+                 new ContextThemeWrapper(mContext, R.style.Theme_AppCompat_DayNight), v);
         popup.getMenu().add(R.string.suggestion_remove).setOnMenuItemClickListener(
                 new MenuItem.OnMenuItemClickListener() {
             @Override
@@ -373,6 +373,12 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
             } else {
                 holder.summary.setText(tile.summary);
                 holder.summary.setVisibility(View.VISIBLE);
+                if (Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.DASHBOARD_SUMMARY_DOUBLE_LINES, 0) == 1) {
+                    holder.summary.setSingleLine(false);
+                } else {
+                    holder.summary.setSingleLine(true);
+                }
             }
         } else {
             holder.summary.setVisibility(View.GONE);
