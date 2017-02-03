24add6719fce2163bf8657f49d20db428bb4133b
ezio84 <brabus84@gmail.com>
2017-02-03 16:40:55 +0100

    Allow to quickly open Running Services from QS panel [2/2]
    Change-Id: I8f1bc70d75aa2f07031f30a2fd408e220b8574e6
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -38,6 +38,7 @@ import com.android.settings.accounts.ManagedProfileSettings;
 import com.android.settings.applications.AppDashboardFragment;
 import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
+import com.android.settings.applications.RunningServices;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.appinfo.AlarmsAndRemindersDetails;
 import com.android.settings.applications.appinfo.AppInfoDashboardFragment;
@@ -318,6 +319,7 @@ public class SettingsGateway {
             MediaManagementAppsDetails.class.getName(),
             // AEX additions
             Extensions.class.getName(),
+            RunningServices.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -359,5 +361,6 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             // AEX additions
             Settings.ExtensionsSettingsActivity.class.getName(),
+            Settings.DevRunningServicesActivity.class.getName()
     };
 }
