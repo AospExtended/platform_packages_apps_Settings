9bb1d2bad98d6c8dd6f6801da3c1f15bd21e0683
Thecrazyskull <anaskarbila@gmail.com>
2017-02-21 17:33:16 -0500

    Settings: Add pocket lock toggle
    Change-Id: I398c8f0be23c27cbcf1fb57eed44a801b31bc6c9
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -71,6 +71,7 @@ import static android.provider.Settings.Secure.CAMERA_GESTURE_DISABLED;
 import static android.provider.Settings.Secure.DOUBLE_TAP_TO_WAKE;
 import static android.provider.Settings.Secure.DOZE_ENABLED;
 import static android.provider.Settings.Secure.WAKE_GESTURE_ENABLED;
+import static android.provider.Settings.System.POCKET_JUDGE;
 import static android.provider.Settings.System.SCREEN_BRIGHTNESS_MODE;
 import static android.provider.Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC;
 import static android.provider.Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL;
@@ -92,6 +93,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_LIFT_TO_WAKE = "lift_to_wake";
     private static final String KEY_DOZE_FRAGMENT = "doze_fragment";
     private static final String KEY_TAP_TO_WAKE = "tap_to_wake";
+    private static final String KEY_POCKET_JUDGE = "pocket_judge";
     private static final String KEY_AUTO_BRIGHTNESS = "auto_brightness";
     private static final String KEY_NIGHT_DISPLAY = "night_display";
     private static final String KEY_DISPLAY_ROTATION = "display_rotation";
@@ -119,6 +121,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private SwitchPreference mLiftToWakePreference;
     private PreferenceScreen mDozeFragment;
     private SwitchPreference mTapToWakePreference;
+    private SwitchPreference mPocketPreference;
     private SwitchPreference mAutoBrightnessPreference;
     private SwitchPreference mCameraGesturePreference;
 
@@ -200,6 +203,9 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                     displayPrefs.removePreference(mTapToWakePreference);
                 }
             }
+
+        mPocketPreference = (SwitchPreference) findPreference(KEY_POCKET_JUDGE);
+        mPocketPreference.setOnPreferenceChangeListener(this);
 
             mCameraGesturePreference = (SwitchPreference) findPreference(KEY_CAMERA_GESTURE);
             if (mCameraGesturePreference != null) {
@@ -451,6 +457,10 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             mTapToWakePreference.setChecked(value != 0);
         }
 
+        if (mPocketPreference != null) {
+            int value = Settings.System.getInt(getContentResolver(), POCKET_JUDGE, 1);
+            mPocketPreference.setChecked(value != 0);
+        }
         // Update camera gesture #1 if it is available.
         if (mCameraGesturePreference != null) {
             int value = Settings.Secure.getInt(getContentResolver(), CAMERA_GESTURE_DISABLED, 0);
@@ -513,6 +523,10 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         if (preference == mTapToWakePreference) {
             boolean value = (Boolean) objValue;
             Settings.Secure.putInt(getContentResolver(), DOUBLE_TAP_TO_WAKE, value ? 1 : 0);
+        }
+        if (preference == mPocketPreference) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getContentResolver(), POCKET_JUDGE, value ? 1 : 0);
         }
         if (preference == mCameraGesturePreference) {
             boolean value = (Boolean) objValue;
