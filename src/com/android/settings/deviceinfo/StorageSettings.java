52d0a2a0b9231e8123fb29817f430d925654a4b6
Bryan Owens <djbryan3540@gmail.com>
2017-02-27 22:24:09 -0600

    Settings: Expose "ALL" hardcoded and @android colors
    Change-Id: Iac46ea78387881e2237c3b9fb302c84987fe94f3
    Signed-off-by: Bryan Owens <djbryan3540@gmail.com>
    
    Conflicts:
    	src/com/android/settings/deviceinfo/StorageSettings.java

--- src/com/android/settings/deviceinfo/StorageSettings.java
+++ src/com/android/settings/deviceinfo/StorageSettings.java
@@ -26,6 +26,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.graphics.PorterDuff;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.UserHandle;
@@ -191,7 +192,8 @@ public class StorageSettings extends SettingsPreferenceFragment implements Index
                 // TODO: add actual storage type to record
                 final Drawable icon = context.getDrawable(R.drawable.ic_sim_sd);
                 icon.mutate();
-                icon.setTint(COLOR_PUBLIC);
+                icon.setTint(getColorPublic(getResources()));
+                icon.setTintMode(PorterDuff.Mode.SRC_ATOP);
 
                 final Preference pref = new Preference(context);
                 pref.setKey(rec.getFsUuid());
