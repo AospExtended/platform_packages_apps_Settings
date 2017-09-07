e2847b1b53fb0ae71013087666958af27e10ea6b
ezio84 <brabus84@gmail.com>
2017-09-07 10:04:35 +0200

    Night Light brightness mode options [2/2]
    Change-Id: I82d9efaadeaf4f847437691ffa8a96d39e82afbf
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- src/com/android/settings/display/NightDisplaySettings.java
+++ src/com/android/settings/display/NightDisplaySettings.java
@@ -20,6 +20,7 @@ import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.preference.DropDownPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.TwoStatePreference;
@@ -46,6 +47,7 @@ public class NightDisplaySettings extends SettingsPreferenceFragment
     private static final String KEY_NIGHT_DISPLAY_END_TIME = "night_display_end_time";
     private static final String KEY_NIGHT_DISPLAY_ACTIVATED = "night_display_activated";
     private static final String KEY_NIGHT_DISPLAY_TEMPERATURE = "night_display_temperature";
+    private static final String KEY_NIGHT_BRIGHTNESS_VALUE = "night_brightness_value";
 
     private static final int DIALOG_START_TIME = 0;
     private static final int DIALOG_END_TIME = 1;
@@ -54,6 +56,7 @@ public class NightDisplaySettings extends SettingsPreferenceFragment
     private DateFormat mTimeFormatter;
 
     private DropDownPreference mAutoModePreference;
+    private DropDownPreference mNightBrightValue;
     private Preference mStartTimePreference;
     private Preference mEndTimePreference;
     private TwoStatePreference mActivatedPreference;
@@ -85,6 +88,7 @@ public class NightDisplaySettings extends SettingsPreferenceFragment
         mEndTimePreference = findPreference(KEY_NIGHT_DISPLAY_END_TIME);
         mActivatedPreference = (TwoStatePreference) findPreference(KEY_NIGHT_DISPLAY_ACTIVATED);
         mTemperaturePreference = (SeekBarPreference) findPreference(KEY_NIGHT_DISPLAY_TEMPERATURE);
+        mNightBrightValue = (DropDownPreference) findPreference(KEY_NIGHT_BRIGHTNESS_VALUE);
 
         mAutoModePreference.setEntries(new CharSequence[] {
                 getString(R.string.night_display_auto_mode_never),
@@ -99,6 +103,12 @@ public class NightDisplaySettings extends SettingsPreferenceFragment
         mAutoModePreference.setOnPreferenceChangeListener(this);
         mActivatedPreference.setOnPreferenceChangeListener(this);
         mTemperaturePreference.setOnPreferenceChangeListener(this);
+
+        int nightBrightValue = Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.NIGHT_BRIGHTNESS_VALUE, 2);
+        mNightBrightValue.setValue(Integer.toString(nightBrightValue));
+        mNightBrightValue.setSummary(mNightBrightValue.getEntry());
+        mNightBrightValue.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -233,6 +243,13 @@ public class NightDisplaySettings extends SettingsPreferenceFragment
             return mController.setActivated((Boolean) newValue);
         } else if (preference == mTemperaturePreference) {
             return mController.setColorTemperature(convertTemperature((Integer) newValue));
+        } else if (preference == mNightBrightValue) {
+            int nightBrightValue = Integer.valueOf((String) newValue);
+            int index = mNightBrightValue.findIndexOfValue((String) newValue);
+            mNightBrightValue.setSummary(mNightBrightValue.getEntries()[index]);
+            Settings.Secure.putInt(getActivity().getContentResolver(),
+                    Settings.Secure.NIGHT_BRIGHTNESS_VALUE, nightBrightValue);
+            return true;
         }
         return false;
     }
