a082eacc2d454e19fb186fb8f7a261ebbf9f9353
Altaf-Mahdi <altaf.mahdi@gmail.com>
2017-10-09 15:52:45 -0400

    Fix refreshing storage summary after force stop
    * if app is force stopped, current implementation causes a nasty loop which in turn causes
      very high cpu usage: refreshUi() > checkForceStop() > mCheckKillProcessesReceiver > refreshUi()
    
    * fix storage not being updated by running refreshUi() in onRunningStateChanged from AppInfoBase
    
    Change-Id: Iec24bb620d1a6287d7ccf583db978ef338ef762e

--- src/com/android/settings/applications/InstalledAppDetails.java
+++ src/com/android/settings/applications/InstalledAppDetails.java
@@ -1472,7 +1472,6 @@ public class InstalledAppDetails extends AppInfoBase
             //Log.d(LOG_TAG, "Got broadcast response: Restart status for "
             //        + mAppEntry.info.packageName + " " + enabled);
             updateForceStopButton(enabled);
-            refreshUi();
         }
     };
 
