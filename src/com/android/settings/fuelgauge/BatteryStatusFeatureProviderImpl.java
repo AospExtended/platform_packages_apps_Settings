5e967fafdd0d3cf2403a977acd353ab606c7f768
Chris <chris@cypheros.co>
2017-12-02 17:05:42 -0500

    fuelgauge: add back battery level animation
    This adds the battery level animation when entering battery
    settings menu. This was dropped in AOSP, but still exists on Pixel's
    (At least in DP4).
    
    When a user enters the battery settings menu, the battery icon will
    animate
    into the current battery level, as well as the battery level text.
    
    This is a revert of: 4a7c8d7
    This is a cherry-pick of: a3c528f
    
    Updated for ODR3 (8.1) by @Sirhc95
    
    @mydongistiny: Fixed for android10
    Change-Id: Ibfe3cc50f192281a0bede670acf9ace1d7523f02
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- src/com/android/settings/fuelgauge/BatteryStatusFeatureProviderImpl.java
+++ src/com/android/settings/fuelgauge/BatteryStatusFeatureProviderImpl.java
@@ -31,7 +31,7 @@ public class BatteryStatusFeatureProviderImpl implements BatteryStatusFeaturePro
 
     @Override
     public boolean triggerBatteryStatusUpdate(
-            BatteryHeaderPreferenceController batteryHeaderPreferenceController, BatteryInfo info) {
+            BatteryInfo info) {
         return false;
     }
 }
