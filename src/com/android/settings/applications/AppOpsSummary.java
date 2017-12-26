rohithbijoy21/packages_apps_Settings

AppOps: use custom arrays and strings without summaries
Change-Id: I63d16aa9b56907b18ed1196f8d76274ebb6bc077

src/com/android/settings/applications/AppOpsSummary.java
@@ -107,7 +107,7 @@ public class AppOpsSummary extends InstrumentedPreferenceFragment {
         mContentContainer = container;
         mRootView = rootView;
 
-        mPageNames = getResources().getTextArray(R.array.app_ops_categories);
+        mPageNames = getResources().getTextArray(R.array.app_ops_categories_custom);
 
         mViewPager = (ViewPager) rootView.findViewById(R.id.pager);
         MyPagerAdapter adapter = new MyPagerAdapter(getChildFragmentManager());

AospExtended/platform_packages_apps_Settings

src/com/android/settings/applications/AppOpsSummary.java
@@ -107,7 +107,7 @@ public class AppOpsSummary extends InstrumentedPreferenceFragment {
         mContentContainer = container;
         mRootView = rootView;
 
-        mPageNames = getResources().getTextArray(R.array.app_ops_categories);
+        mPageNames = getResources().getTextArray(R.array.app_ops_categories_aosp);
 
         mViewPager = (ViewPager) rootView.findViewById(R.id.pager);
         MyPagerAdapter adapter = new MyPagerAdapter(getChildFragmentManager());