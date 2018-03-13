209cae77467f35d4f5b923431576e100232ab740
Akhil Narang <akhilnarang.1999@gmail.com>
2018-03-13 16:58:36 +0530

    ProximityOnWakePreferenceController: implement PreferenceControllerMixin
      * Fixes Settings Search FC
    
       03-13 10:25:40.370 E/AndroidRuntime(5419): Caused by: java.lang.IllegalStateException: com.android.settings.display.ProximityOnWakePreferenceController must implement com.android.settings.core.PreferenceControllerMixin
    
    Change-Id: Iaaee49d093db420fd9a1b4ca29ddcfb77dc55557
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- src/com/android/settings/display/ProximityOnWakePreferenceController.java
+++ src/com/android/settings/display/ProximityOnWakePreferenceController.java
@@ -20,9 +20,10 @@ import android.support.v7.preference.Preference;
 
 import com.android.settingslib.core.AbstractPreferenceController;
 import org.aospextended.extensions.preference.SystemSettingSwitchPreference;
+import com.android.settings.core.PreferenceControllerMixin;
 
 public class ProximityOnWakePreferenceController extends AbstractPreferenceController implements
-        Preference.OnPreferenceChangeListener {
+        Preference.OnPreferenceChangeListener, PreferenceControllerMixin {
 
     private static final String KEY_PROXIMITY_WAKE = "proximity_on_wake";
 
