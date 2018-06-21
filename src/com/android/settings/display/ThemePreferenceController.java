ca2a51ca13d982332fedf9d3a9bc4c93f30265b7
drpradsms <dr.pradsms@gmail.com>
2018-06-21 13:00:39 +0530

    Chocolate UI [3/4]
    Base theme: ExtendedUI
    
    Change-Id: Id8d618b929bf73059a7022c0ff36820804623a3d

--- src/com/android/settings/display/ThemePreferenceController.java
+++ src/com/android/settings/display/ThemePreferenceController.java
@@ -151,7 +151,8 @@ public class ThemePreferenceController extends AbstractPreferenceController impl
                         isChangeableOverlay(infos.get(i).packageName) &&
                         !infos.get(i).packageName.equals("com.android.system.theme.dark") &&
                         !infos.get(i).packageName.equals("com.android.system.theme.black") &&
-                        !infos.get(i).packageName.equals("com.android.system.theme.extended")) {
+                        !infos.get(i).packageName.equals("com.android.system.theme.extended") &&
+                        !infos.get(i).packageName.equals("com.android.system.theme.chocolate")) {
                     return infos.get(i).packageName;
                 }
             }
@@ -184,7 +185,8 @@ public class ThemePreferenceController extends AbstractPreferenceController impl
                 if (isChangeableOverlay(infos.get(i).packageName)) {
                     if (!infos.get(i).packageName.equals("com.android.system.theme.dark") && 
                         !infos.get(i).packageName.equals("com.android.system.theme.black") &&
-                        !infos.get(i).packageName.equals("com.android.system.theme.extended"))
+                        !infos.get(i).packageName.equals("com.android.system.theme.extended") &&
+                        !infos.get(i).packageName.equals("com.android.system.theme.chocolate"))
                         pkgs.add(infos.get(i).packageName);
                 }
             }
