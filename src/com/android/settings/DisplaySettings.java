f7b7b928a466d7669183aa7cad478412865030f7
Shubham Singh <coolsks94@gmail.com>
2018-06-27 23:11:24 +0530

    Allow users to use system themes/fonts alongside subs [1/2]
     Themes,accent and font prefs are disabled if substratum is installed.
     User might just need to use substratum to enable round corner, use custom navbar
     icons,use custom accent etc alongside system theme. Hence allow users to use them alongside substratum.
     Show a proper warning to user on enabling this.
    
    Change-Id: I98d60486e3df64cee468281297ccae1941d114ca
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -36,6 +36,7 @@ import com.android.settings.display.CameraGesturePreferenceController;
 import com.android.settings.display.ColorModePreferenceController;
 import com.android.settings.display.FontPickerPreferenceController;
 import com.android.settings.display.FontSizePreferenceController;
+import com.android.settings.display.ForceAllowThemePreferenceController;
 import com.android.settings.display.LiftToWakePreferenceController;
 import com.android.settings.display.NightDisplayPreferenceController;
 import com.android.settings.display.NightModePreferenceController;
@@ -54,6 +55,7 @@ import com.android.settingslib.core.lifecycle.Lifecycle;
 
 import java.util.ArrayList;
 import java.util.List;
+import android.util.Log;
 
 public class DisplaySettings extends DashboardFragment {
     private static final String TAG = "DisplaySettings";
@@ -66,6 +68,8 @@ public class DisplaySettings extends DashboardFragment {
 
     private IntentFilter mIntentFilter;
     private static FontPickerPreferenceController mFontPickerPreference;
+    private static AccentPickerPreferenceController mAccentPickerPreference;
+    private static DarkUIPreferenceController mUIStylePreference;
 
     private BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
         @Override
@@ -134,7 +138,7 @@ public class DisplaySettings extends DashboardFragment {
             Context context, Lifecycle lifecycle, Fragment fragment) {
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
         controllers.add(mFontPickerPreference = new FontPickerPreferenceController(context, lifecycle, fragment));
-        controllers.add(new AccentPickerPreferenceController(context, lifecycle, fragment));
+        controllers.add(mAccentPickerPreference = new AccentPickerPreferenceController(context, lifecycle, fragment));
         controllers.add(new AutoBrightnessPreferenceController(context, KEY_AUTO_BRIGHTNESS));
         // controllers.add(new AutoRotatePreferenceController(context, lifecycle));
         controllers.add(new CameraGesturePreferenceController(context));
@@ -153,7 +157,8 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new VrDisplayPreferenceController(context));
         controllers.add(new WallpaperPreferenceController(context));
         controllers.add(new ThemePreferenceController(context));
-        controllers.add(new DarkUIPreferenceController(context));
+        controllers.add(mUIStylePreference = new DarkUIPreferenceController(context, lifecycle, fragment));
+        controllers.add(new ForceAllowThemePreferenceController(context, mUIStylePreference, mAccentPickerPreference, mFontPickerPreference));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
         controllers.add(new ColorModePreferenceController(context));
         return controllers;
