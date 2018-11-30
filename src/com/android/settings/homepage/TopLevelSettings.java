04eb0b11a190a403cd8917edd8d47ab44f7f4a8b
SpiritCroc <dev@spiritcroc.de>
2018-11-30 10:58:57 +0100

    Theme settings dashboard icons [2/3]
    Not using overlays since third party apps can add preferences including
    icon to dashboard as well.
    
    Change-Id: I485d0d17297c165b8b0de67915355338166cad51
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- src/com/android/settings/homepage/TopLevelSettings.java
+++ src/com/android/settings/homepage/TopLevelSettings.java
@@ -21,10 +21,20 @@ import static com.android.settingslib.search.SearchIndexable.MOBILE;
 
 import android.app.settings.SettingsEnums;
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.AdaptiveIconDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.ShapeDrawable;
 import android.os.Bundle;
+import android.provider.Settings;
 
+import androidx.core.graphics.ColorUtils;
 import androidx.fragment.app.Fragment;
 import androidx.preference.Preference;
+import androidx.preference.PreferenceGroup;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.android.settings.R;
@@ -34,12 +44,24 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.support.SupportPreferenceController;
 import com.android.settingslib.core.instrumentation.Instrumentable;
 import com.android.settingslib.search.SearchIndexable;
+import com.android.settingslib.widget.AdaptiveIcon;
+
+import com.android.settingslib.widget.AdaptiveIcon;
+import com.android.settingslib.widget.AdaptiveIconShapeDrawable;
 
 @SearchIndexable(forTarget = MOBILE)
 public class TopLevelSettings extends DashboardFragment implements
         PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
 
     private static final String TAG = "TopLevelSettings";
+
+    private int mIconStyle;
+    private int mNormalColor;
+    private int mAccentColor;
+    private int mPrimaryColor;
+
+    private Drawable fg;
+    private Drawable bg;
 
     public TopLevelSettings() {
         final Bundle args = new Bundle();
@@ -108,4 +130,112 @@ public class TopLevelSettings extends DashboardFragment implements
                     return false;
                 }
             };
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateTheme();
+    }
+
+    private void updateTheme() {
+        int[] attrs = new int[] {
+            android.R.attr.colorControlNormal,
+            android.R.attr.colorAccent,
+            android.R.attr.colorPrimary,
+        };
+        TypedArray ta = getContext().getTheme().obtainStyledAttributes(attrs);
+        mNormalColor = ta.getColor(0, 0xff808080);
+        mAccentColor = ta.getColor(1, 0xff808080);
+        mPrimaryColor = ta.getColor(2, 0xff808080);
+        ta.recycle();
+
+        mIconStyle = Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.THEMING_SETTINGS_DASHBOARD_ICONS, 0);
+        themePreferences(getPreferenceScreen());
+    }
+
+    private void themePreferences(PreferenceGroup prefGroup) {
+        themePreference(prefGroup);
+        for (int i = 0; i < prefGroup.getPreferenceCount(); i++) {
+            Preference pref = prefGroup.getPreference(i);
+            if (pref instanceof PreferenceGroup) {
+                themePreferences(prefGroup);
+            } else {
+                themePreference(pref);
+            }
+        }
+    }
+
+    private void themePreference(Preference pref) {
+        Drawable icon = pref.getIcon();
+        int bgc = 0;
+        if (icon != null) {
+            if (icon instanceof AdaptiveIcon) {
+                AdaptiveIcon aIcon = (AdaptiveIcon) icon;
+                bgc = aIcon.getBackgroundColor();
+       // Clear colors from previous calls
+        aIcon.resetColors();
+        switch (mIconStyle) {
+            case 0:
+                aIcon.setForegroundColor(mPrimaryColor);
+                break;
+            case 1:
+                aIcon.setForegroundColor(mPrimaryColor);
+                aIcon.setCustomBackgroundColor(mAccentColor);
+                break;
+            case 2:
+                aIcon.setForegroundColor(mNormalColor);
+                aIcon.setCustomBackgroundColor(0);
+                break;
+            case 3:
+                aIcon.setForegroundColor(mAccentColor);
+                aIcon.setCustomBackgroundColor(0);
+                break;
+            case 4:
+                aIcon.setForegroundColor(mAccentColor);
+                aIcon.setCustomBackgroundColor(ColorUtils.setAlphaComponent(mAccentColor, 51));
+                break;
+            case 5:
+                aIcon.setForegroundColor(bgc);
+                aIcon.setCustomBackgroundColor(ColorUtils.setAlphaComponent(bgc, 51));
+                break;
+        }
+            } else if (icon instanceof LayerDrawable) {
+                LayerDrawable lIcon = (LayerDrawable) icon;
+                if (lIcon.getNumberOfLayers() == 2) {
+                    fg = lIcon.getDrawable(1);
+                    bg = lIcon.getDrawable(0);
+                    bgc = ((ShapeDrawable) bg).getPaint().getColor();
+                    // Clear tints from previous calls
+                    bg.setTintList(null);
+                    fg.setTintList(null);
+                    switch (mIconStyle) {
+                        case 0:
+                            fg.setTint(mPrimaryColor);
+                            break;
+                        case 1:
+                            fg.setTint(mPrimaryColor);
+                            bg.setTint(mAccentColor);
+                            break;
+                        case 2:
+                            fg.setTint(mNormalColor);
+                            bg.setTint(0);
+                            break;
+                        case 3:
+                            fg.setTint(mAccentColor);
+                            bg.setTint(0);
+                            break;
+                        case 4:
+                            fg.setTint(mAccentColor);
+                            bg.setTint(ColorUtils.setAlphaComponent(mAccentColor, 51));
+                            break;
+                        case 5:
+                            fg.setTint(bgc);
+                            bg.setTint(ColorUtils.setAlphaComponent(bgc, 51));
+                            break;
+                    }
+                }
+            }
+        }
+    }
 }
