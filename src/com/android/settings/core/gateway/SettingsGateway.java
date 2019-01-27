271d72f8b863f7b1f06325b59bf0003688a98f7f
PMS22 <prathams99@rediff.com>
2019-01-27 08:40:04 +0000

    Settings: Gaming Mode revamped [3/4]
    Change-Id: Id3189e61044080f343cc8285370c5d79bd44d83d
    Signed-off-by: PMS22 <prathams99@rediff.com>
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -146,6 +146,7 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 import org.aospextended.extensions.Extensions;
 import org.aospextended.extensions.fragments.HeadsUpSettings;
 import org.aospextended.extensions.fragments.SmartPixels;
+import org.aospextended.extensions.fragments.GamingMode;
 
 import com.android.settings.livedisplay.LiveDisplaySettings;
 
@@ -283,6 +284,7 @@ public class SettingsGateway {
             RunningServices.class.getName(),
             SmartPixels.class.getName(),
             LiveDisplaySettings.class.getName(),
+            GamingMode.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
