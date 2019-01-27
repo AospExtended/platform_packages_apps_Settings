05895764a36ea3c29ea514413150175df24838fd
PMS22 <prathams99@rediff.com>
2019-01-27 08:40:04 +0000

    Settings: Gaming Mode revamped [3/3]
    Change-Id: Id3189e61044080f343cc8285370c5d79bd44d83d
    Signed-off-by: PMS22 <prathams99@rediff.com>
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -188,4 +188,5 @@ public class Settings extends SettingsActivity {
 
     // LiveDisplay
     public static class LiveDisplaySettingsActivity extends SettingsActivity { /* empty */ }
+    public static class GamingSettingsActivity extends SettingsActivity { /* empty */ }
 }
