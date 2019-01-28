LineageOS/android_packages_apps_Settings

Settings: Check interfaces before enabling ADB over network
 * Make sure that an active connection is established in
   one of the supported interfaces - WiFi or USB network.

Change-Id: I6c5c468f1b58aefe64065c373968cae0e700e415

src/com/android/settings/development/WirelessAdbPreferenceController.java
@@ -17,6 +17,8 @@
 package com.android.settings.development;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.NetworkUtils;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
@@ -96,9 +98,17 @@ public class WirelessAdbPreferenceController extends DeveloperOptionsPreferenceC
         }
     }
 
+    private boolean isConnectedToSupportedInterface() {
+        ConnectivityManager connMgr = mContext.getSystemService(ConnectivityManager.class);
+        NetworkInfo wiredNwInfo = connMgr.getNetworkInfo(ConnectivityManager.TYPE_ETHERNET);
+        NetworkInfo wirelessNwInfo = connMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        return (wiredNwInfo != null && wiredNwInfo.isConnected()) ||
+                (wirelessNwInfo != null && wirelessNwInfo.isConnected());
+    }
+
     public void onWirelessAdbDialogConfirmed() {
         LineageSettings.Secure.putInt(mContext.getContentResolver(),
-                LineageSettings.Secure.ADB_PORT, 5555);
+                LineageSettings.Secure.ADB_PORT, !isConnectedToSupportedInterface() ? -1 : 5555);
         updatePreference();
     }
 

AospExtended/platform_packages_apps_Settings

Signed-off-by: mydongistiny <jaysonedson@gmail.com>

src/com/android/settings/development/WirelessAdbPreferenceController.java
@@ -17,6 +17,8 @@
 package com.android.settings.development;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.NetworkUtils;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
@@ -96,9 +98,17 @@ public class WirelessAdbPreferenceController extends DeveloperOptionsPreferenceC
         }
     }
 
+    private boolean isConnectedToSupportedInterface() {
+        ConnectivityManager connMgr = mContext.getSystemService(ConnectivityManager.class);
+        NetworkInfo wiredNwInfo = connMgr.getNetworkInfo(ConnectivityManager.TYPE_ETHERNET);
+        NetworkInfo wirelessNwInfo = connMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        return (wiredNwInfo != null && wiredNwInfo.isConnected()) ||
+                (wirelessNwInfo != null && wirelessNwInfo.isConnected());
+    }
+
     public void onWirelessAdbDialogConfirmed() {
         Settings.Secure.putInt(mContext.getContentResolver(),
-                Settings.Secure.ADB_PORT, 5555);
+                Settings.Secure.ADB_PORT, !isConnectedToSupportedInterface() ? -1 : 5555);
         updatePreference();
     }
 