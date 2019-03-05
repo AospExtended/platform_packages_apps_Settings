6527553e1c749b5c8aa8990f97ef7dbb5da3eda5
theimpulson <aayushgupta219@gmail.com>
2019-03-05 02:34:09 +0000

    Settings: Remove extended version from device info screen
    *It's already in firmware dialog screen
    
    Change-Id: Ib1fe3dd7d2276dfa1cf2ad2f11cd782acd49832a

--- src/com/android/settings/deviceinfo/DeviceInfoSettings.java
+++ src/com/android/settings/deviceinfo/DeviceInfoSettings.java
@@ -32,7 +32,6 @@ import com.android.settings.deviceinfo.firmwareversion.FirmwareVersionPreference
 import com.android.settings.deviceinfo.imei.ImeiInfoPreferenceController;
 import com.android.settings.deviceinfo.simstatus.SimStatusPreferenceController;
 import com.android.settings.deviceinfo.BuildDatePreferenceController;
-import com.android.settings.deviceinfo.ExtendedVersionPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settingslib.core.AbstractPreferenceController;
@@ -128,7 +127,6 @@ public class DeviceInfoSettings extends DashboardFragment implements Indexable {
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
         controllers.add(new PhoneNumberPreferenceController(context));
         controllers.add(new SimStatusPreferenceController(context, fragment));
-        controllers.add(new ExtendedVersionPreferenceController(context, lifecycle));
         controllers.add(new BuildDatePreferenceController(context));
         controllers.add(new DeviceModelPreferenceController(context, fragment));
         controllers.add(new ImeiInfoPreferenceController(context, fragment));
