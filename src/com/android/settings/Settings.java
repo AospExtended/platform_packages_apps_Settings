7b34970c9e596349d344c1f1b416236c578eb99a
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-03 14:18:10 -0300

    Settings: Changes for LiveDisplay
    All credits to LineageOS and related authors on:
    
    https://github.com/LineageOS/android_packages_apps_LineageParts
    
    Syberia: Forward port to 10, 11, 12 and migrate to androidx
    
    Change-Id: Ibf12e0141947cbffa74b3c005d10efc11dfc591d
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -332,4 +332,8 @@ public class Settings extends SettingsActivity {
     public static class DevRunningServicesActivity extends SettingsActivity { /* empty */ }
 
     public static class HeadsUpSettingsActivity extends SettingsActivity {}
+
+    // LiveDisplay
+    public static class LiveDisplaySettingsActivity extends SettingsActivity { /* empty */ }
+
 }
