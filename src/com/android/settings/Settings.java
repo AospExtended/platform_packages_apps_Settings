86a7b2bb2579bf7a7fac10481b75d78345c91475
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-03 14:18:10 -0300

    Settings: Changes for LiveDisplay
    All credits to LineageOS and related authors on:
    
    https://github.com/LineageOS/android_packages_apps_LineageParts
    
    Syberia: Forward port to 10 and migrate to androidx
    
    Change-Id: Ibf12e0141947cbffa74b3c005d10efc11dfc591d
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -185,4 +185,7 @@ public class Settings extends SettingsActivity {
     // Gaming Mode
     public static class GamingModeActivity extends SettingsActivity {}
 
+    // LiveDisplay
+    public static class LiveDisplaySettingsActivity extends SettingsActivity { /* empty */ }
+
 }
