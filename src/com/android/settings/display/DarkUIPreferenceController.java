3e1f1b9ee0431948fd721894f4b55076f5324b92
Shubham Singh <coolsks94@gmail.com>
2019-05-13 16:20:34 +0530

    Theming: Refactor [2/3]
    Change-Id: I8a8ba3d07eb70176d554cb21cc62a9e5e69be548

--- src/com/android/settings/display/DarkUIPreferenceController.java
+++ src/com/android/settings/display/DarkUIPreferenceController.java
@@ -1,62 +1,63 @@
 /*
- * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2018 The Android Open Source Project
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the
- * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
  */
+
 package com.android.settings.display;
 
-import android.app.Fragment;
+import android.app.UiModeManager;
 import android.content.Context;
-import android.content.ContentResolver;
-import android.os.Bundle;
-import android.text.TextUtils;
+import android.provider.Settings;
+import android.support.annotation.VisibleForTesting;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
-import android.provider.Settings;
-
-import static com.android.settings.display.ThemeUtils.isSubstratumOverlayInstalled;
 
 import com.android.settingslib.core.AbstractPreferenceController;
-import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.core.lifecycle.LifecycleObserver;
 import com.android.settingslib.core.lifecycle.events.OnResume;
-import com.android.settingslib.drawer.SettingsDrawerActivity;
-
-import libcore.util.Objects;
-import java.util.ArrayList;
-import java.util.List;
 
 import com.android.settings.R;
-import android.content.Intent;
-import android.os.Handler;
-import android.widget.Toast;
+import com.android.settings.core.PreferenceControllerMixin;
 
-public class DarkUIPreferenceController extends AbstractPreferenceController implements
-        PreferenceControllerMixin, Preference.OnPreferenceChangeListener, LifecycleObserver, OnResume {
+import static com.android.settings.display.ThemeUtils.isSubstratumOverlayInstalled;
 
-    private static final String SYSTEM_THEME_STYLE = "system_theme_style";
+public class DarkUIPreferenceController extends AbstractPreferenceController
+        implements Preference.OnPreferenceChangeListener, PreferenceControllerMixin, LifecycleObserver, OnResume {
+
+    private static final String DARK_UI_KEY = "dark_ui_mode";
+    private final UiModeManager mUiModeManager;
     private ListPreference mSystemThemeStyle;
 
-    public DarkUIPreferenceController(Context context, Lifecycle lifecycle, Fragment parent) {
+    public DarkUIPreferenceController(Context context, Lifecycle lifecycle) {
         super(context);
+        mUiModeManager = context.getSystemService(UiModeManager.class);
         if (lifecycle != null) {
             lifecycle.addObserver(this);
         }
     }
 
+    @VisibleForTesting
+    DarkUIPreferenceController(Context context, UiModeManager uiModeManager) {
+        super(context);
+        mUiModeManager = uiModeManager;
+    }
+
     @Override
     public String getPreferenceKey() {
-        return SYSTEM_THEME_STYLE;
+        return DARK_UI_KEY;
     }
 
     @Override
@@ -67,28 +68,72 @@ public class DarkUIPreferenceController extends AbstractPreferenceController imp
     @Override
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
-        mSystemThemeStyle = (ListPreference) screen.findPreference(SYSTEM_THEME_STYLE);
+        mSystemThemeStyle = (ListPreference) screen.findPreference(DARK_UI_KEY);
         updateState();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if(preference == mSystemThemeStyle) {
+            mUiModeManager.setNightMode(modeToInt((String) newValue));
+            updateSummary();
+        }
+        return true;
     }
 
     public void updateState() {
         if (!isSubstratumOverlayInstalled(mContext) || isForceThemeAllowed()) {
-            int systemThemeStyle = Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.SYSTEM_THEME_STYLE, 0);
-            int valueIndex = mSystemThemeStyle.findIndexOfValue(String.valueOf(systemThemeStyle));
-            mSystemThemeStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
-            mSystemThemeStyle.setSummary(mSystemThemeStyle.getEntry());
-            mSystemThemeStyle.setOnPreferenceChangeListener(this);
             mSystemThemeStyle.setEnabled(true);
+            updateSummary();
         } else {
             mSystemThemeStyle.setEnabled(false);
             mSystemThemeStyle.setSummary(R.string.substratum_installed_title);
         }
     }
 
-    @Override
-    public void onResume() {
-        updateState();
+    private void updateSummary() {
+        int mode = mUiModeManager.getNightMode();
+        mSystemThemeStyle.setValue(modeToString(mode));
+        mSystemThemeStyle.setSummary(modeToDescription(mode));
+    }
+
+    private String modeToDescription(int mode) {
+        String[] values = mContext.getResources().getStringArray(R.array.dark_ui_mode_entries);
+        switch (mode) {
+            case UiModeManager.MODE_NIGHT_AUTO:
+                return values[0];
+            case UiModeManager.MODE_NIGHT_YES:
+                return values[1];
+            case UiModeManager.MODE_NIGHT_NO:
+            default:
+                return values[2];
+
+        }
+    }
+
+    private String modeToString(int mode) {
+        switch (mode) {
+            case UiModeManager.MODE_NIGHT_AUTO:
+                return "auto";
+            case UiModeManager.MODE_NIGHT_YES:
+                return "yes";
+            case UiModeManager.MODE_NIGHT_NO:
+            default:
+                return "no";
+
+        }
+    }
+
+    private int modeToInt(String mode) {
+        switch (mode) {
+            case "auto":
+                return UiModeManager.MODE_NIGHT_AUTO;
+            case "yes":
+                return UiModeManager.MODE_NIGHT_YES;
+            case "no":
+            default:
+                return UiModeManager.MODE_NIGHT_NO;
+        }
     }
 
     public boolean isForceThemeAllowed() {
@@ -97,37 +142,7 @@ public class DarkUIPreferenceController extends AbstractPreferenceController imp
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mSystemThemeStyle) {
-            String value = (String) newValue;
-            Settings.System.putInt(mContext.getContentResolver(), Settings.System.SYSTEM_THEME_STYLE, Integer.valueOf(value));
-            int valueIndex = mSystemThemeStyle.findIndexOfValue(value);
-            mSystemThemeStyle.setSummary(mSystemThemeStyle.getEntries()[valueIndex]);
-            try {
-                reload();
-            }catch (Exception ignored){
-            }
-        }
-        return true;
-    }
-    private void reload(){
-        Intent intent2 = new Intent(Intent.ACTION_MAIN);
-        intent2.addCategory(Intent.CATEGORY_HOME);
-        intent2.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        mContext.startActivity(intent2);
-        Toast.makeText(mContext, R.string.applying_theme_toast, Toast.LENGTH_SHORT).show();
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-              @Override
-              public void run() {
-                  Intent intent = new Intent(Intent.ACTION_MAIN);
-                  intent.setClassName("com.android.settings",
-                        "com.android.settings.Settings$DisplaySettingsActivity");
-                  intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-                  intent.putExtra(SettingsDrawerActivity.EXTRA_SHOW_MENU, true);
-                  mContext.startActivity(intent);
-                  Toast.makeText(mContext, R.string.theme_applied_toast, Toast.LENGTH_SHORT).show();
-              }
-        }, 3000);
+    public void onResume() {
+        updateState();
     }
 }
