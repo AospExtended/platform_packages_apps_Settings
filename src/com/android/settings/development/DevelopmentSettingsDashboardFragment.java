56445ea59ec7e8ea93a6c3b701c519c4fc44a9b1
SagarMakhar <sagarmakhar@gmail.com>
2019-09-11 15:30:58 +0000

    Add theming settings back
    Change-Id: Ibd597fbabf0fe624b40730d2168aa08b93babcad
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
+++ src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
@@ -577,7 +577,6 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
                 bluetoothA2dpConfigStore, fragment));
         controllers.add(new SharedDataPreferenceController(context));
         controllers.add(new OverlaySettingsPreferenceController(context));
-
         return controllers;
     }
 
