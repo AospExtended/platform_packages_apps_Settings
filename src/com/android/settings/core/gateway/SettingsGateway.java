63c41e28723e0464bbf99983d31af5a3ab5186f0
Pranav Vashi <neobuddy89@gmail.com>
2019-11-17 02:30:00 +0530

    Bring back Gaming mode [2/3]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: Ibc64817badd1fe3ed42165588b8939a50b86d35c

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -150,6 +150,7 @@ import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import org.aospextended.extensions.Extensions;
+import org.aospextended.extensions.fragments.GamingMode;
 
 public class SettingsGateway {
 
@@ -289,8 +290,8 @@ public class SettingsGateway {
             // AEX additions
             Extensions.class.getName(),
             RunningServices.class.getName(),
-
-            DarkModeSettingsFragment.class.getName()
+            DarkModeSettingsFragment.class.getName(),
+            GamingMode.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -331,6 +332,7 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             // AEX additions
             Settings.ExtensionsSettingsActivity.class.getName(),
-            Settings.DevRunningServicesActivity.class.getName()
+            Settings.DevRunningServicesActivity.class.getName(),
+            Settings.GamingModeActivity.class.getName(),
     };
 }
