4aae194e033ee27e220f0c8898747b440fe9150b
Mazda-- <alex@dirtyunicorns.com>
2020-01-20 22:28:51 +0530

    Themes: Add More System Themes [3/3]
    SDB: Adapt for AOSP Settings
    
    Special Thanks to @imjyotiraditya for the help
    
    Change-Id: I9aa45710d53ba4f3bfcffbb820fcc1b0f13bdb3e

---
+++ src/com/android/settings/Themes.java
@@ -0,0 +1,142 @@
+package com.android.settings;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import static android.os.UserHandle.USER_SYSTEM;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.UiModeManager;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.content.om.IOverlayManager;
+import android.content.om.OverlayInfo;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.SystemProperties;
+import android.os.ServiceManager;
+import android.os.UserHandle;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+import android.provider.Settings;
+import com.android.settings.R;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Objects;
+
+import com.android.internal.util.aospextended.ThemesUtils;
+import com.android.internal.util.aospextended.AEXUtils;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class Themes extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String PREF_THEME_SWITCH = "theme_switch";
+
+    private IOverlayManager mOverlayService;
+    private UiModeManager mUiModeManager;
+    private ListPreference mThemeSwitch;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.display_settings);
+
+        mUiModeManager = getContext().getSystemService(UiModeManager.class);
+
+        mOverlayService = IOverlayManager.Stub
+                .asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
+
+        setupThemeSwitchPref();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mThemeSwitch) {
+            String theme_switch = (String) objValue;
+            final Context context = getContext();
+            switch (theme_switch) {
+                case "1":
+                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_NO, ThemesUtils.PITCH_BLACK);
+                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_NO, ThemesUtils.EXTENDED_UI);
+                    break;
+                case "2":
+                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.PITCH_BLACK);
+                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.EXTENDED_UI);
+                    break;
+                case "3":
+                    handleBackgrounds(true, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.PITCH_BLACK);
+                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.EXTENDED_UI);
+                    break;
+                case "4":
+                    handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.PITCH_BLACK);
+                    handleBackgrounds(true, context, UiModeManager.MODE_NIGHT_YES, ThemesUtils.EXTENDED_UI);
+                    setAccent("com.android.theme.color.extendedgreen", extendedGreenAccent);
+                    break;
+            }
+            try {
+                 mOverlayService.reloadAndroidAssets(UserHandle.USER_CURRENT);
+                 mOverlayService.reloadAssets("com.android.settings", UserHandle.USER_CURRENT);
+                 mOverlayService.reloadAssets("com.android.systemui", UserHandle.USER_CURRENT);
+             } catch (RemoteException ignored) {
+             }
+        }
+        return true;
+    }
+
+    private void setupThemeSwitchPref() {
+        mThemeSwitch = (ListPreference) findPreference(PREF_THEME_SWITCH);
+        mThemeSwitch.setOnPreferenceChangeListener(this);
+        if (AEXUtils.isThemeEnabled("com.android.theme.extendedui.system")) {
+            mThemeSwitch.setValue("4");
+        } else if (AEXUtils.isThemeEnabled("com.android.theme.pitchblack.system")) {
+            mThemeSwitch.setValue("3");
+        } else if (mUiModeManager.getNightMode() == UiModeManager.MODE_NIGHT_YES) {
+            mThemeSwitch.setValue("2");
+        } else {
+            mThemeSwitch.setValue("1");
+        }
+    }
+
+    private void handleBackgrounds(Boolean state, Context context, int mode, String[] overlays) {
+        if (context != null) {
+            Objects.requireNonNull(context.getSystemService(UiModeManager.class))
+                    .setNightMode(mode);
+        }
+        for (int i = 0; i < overlays.length; i++) {
+            String background = overlays[i];
+            try {
+                mOverlayService.setEnabled(background, state, USER_SYSTEM);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void setAccent(final String accent, final Button buttonAccent) {
+        for (int i = 0; i < ThemesUtils.ACCENTS.length; i++) {
+            String accent = ThemesUtils.ACCENTS[i];
+            try {
+                mOverlayManager.setEnabled(accent, true, USER_SYSTEM);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.EXTENSIONS;
+    }
+}
