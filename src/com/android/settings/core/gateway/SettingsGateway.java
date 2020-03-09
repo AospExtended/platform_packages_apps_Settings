5fd58987f0b82cbe15abaa0023c8d854684f2e86
SagarMakhar <sagarmakhar@gmail.com>
2020-03-09 10:38:14 +0000

    Merged tag 'android-10.0.0_r32' into 10.x
    Change-Id: I6833f722bf95c4fcde9203705f6ff5084040b0d4
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -75,6 +75,7 @@ import com.android.settings.deviceinfo.aboutphone.MyDeviceInfoFragment;
 import com.android.settings.deviceinfo.firmwareversion.FirmwareVersionSettings;
 import com.android.settings.deviceinfo.legal.ModuleLicensesDashboard;
 import com.android.settings.display.NightDisplaySettings;
+import com.android.settings.display.darkmode.DarkModeSettingsFragment;
 import com.android.settings.dream.DreamSettings;
 import com.android.settings.enterprise.EnterprisePrivacySettings;
 import com.android.settings.fuelgauge.AdvancedPowerUsageDetail;
@@ -287,7 +288,9 @@ public class SettingsGateway {
             GlobalActionsPanelSettings.class.getName(),
             // AEX additions
             Extensions.class.getName(),
-            RunningServices.class.getName()
+            RunningServices.class.getName(),
+
+            DarkModeSettingsFragment.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
