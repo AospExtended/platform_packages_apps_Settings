3de4bee84ee4deb0dfa7b6aed54f5ec3f76bd509
Arian <arian.kulmer@web.de>
2020-07-29 16:05:53 +0200

    Settings: Add a toggle for anti flicker mode in LiveDisplay settings
    Change-Id: I4a80af3888c578652bf5e264a62e333cc2162305
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- src/com/android/settings/livedisplay/LiveDisplaySettings.java
+++ src/com/android/settings/livedisplay/LiveDisplaySettings.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2015 The CyanogenMod Project
- *               2017-2019 The LineageOS Project
+ *               2017-2012 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -54,6 +54,7 @@ import com.android.settings.custom.utils.ResourceUtils;
 
 import com.android.settings.custom.preference.CustomDialogPreference;
 
+import static com.android.internal.custom.hardware.LiveDisplayManager.FEATURE_ANTI_FLICKER;
 import static com.android.internal.custom.hardware.LiveDisplayManager.FEATURE_CABC;
 import static com.android.internal.custom.hardware.LiveDisplayManager.FEATURE_COLOR_ADJUSTMENT;
 import static com.android.internal.custom.hardware.LiveDisplayManager.FEATURE_COLOR_ENHANCEMENT;
@@ -75,6 +76,7 @@ public class LiveDisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_CATEGORY_ADVANCED = "advanced";
 
     private static final String KEY_LIVE_DISPLAY = "live_display";
+    private static final String KEY_LIVE_DISPLAY_ANTI_FLICKER = "display_anti_flicker";
     private static final String KEY_LIVE_DISPLAY_AUTO_OUTDOOR_MODE =
             "display_auto_outdoor_mode";
     private static final String KEY_LIVE_DISPLAY_READING_ENHANCEMENT = "display_reading_mode";
@@ -102,6 +104,7 @@ public class LiveDisplaySettings extends SettingsPreferenceFragment implements
 
     private ListPreference mLiveDisplay;
 
+    private SwitchPreference mAntiFlicker;
     private SwitchPreference mColorEnhancement;
     private SwitchPreference mLowPower;
     private SwitchPreference mOutdoorMode;
@@ -256,6 +259,13 @@ public class LiveDisplaySettings extends SettingsPreferenceFragment implements
                 !mConfig.hasFeature(FEATURE_COLOR_ADJUSTMENT)) {
             advancedPrefs.removePreference(mDisplayColor);
             mDisplayColor = null;
+        }
+
+        mAntiFlicker = findPreference(KEY_LIVE_DISPLAY_ANTI_FLICKER);
+        if (liveDisplayPrefs != null && mAntiFlicker != null &&
+                !mHardware.isSupported(LineageHardwareManager.FEATURE_ANTI_FLICKER)) {
+            liveDisplayPrefs.removePreference(mAntiFlicker);
+            mAntiFlicker = null;
         }
     }
 
@@ -461,6 +471,10 @@ public class LiveDisplaySettings extends SettingsPreferenceFragment implements
                 }
                 result.add(KEY_LIVE_DISPLAY_TEMPERATURE);
             }
+            if (!config.hasFeature(FEATURE_ANTI_FLICKER)) {
+                result.add(KEY_LIVE_DISPLAY_ANTI_FLICKER);
+            }
+
             return result;
         }
 
