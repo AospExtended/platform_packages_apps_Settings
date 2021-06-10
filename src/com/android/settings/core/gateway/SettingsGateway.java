BenzoRom/packages_apps_Settings

Settings: Live Volume Steps [2/3]
Change-Id: I1cf0d9e5933a846600a723cb7720d47d3bd2d22a
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -150,6 +150,7 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.benzo.settings.BenzoSettings;
 import com.benzo.settings.GlobalActionsSettings;
+import com.benzo.settings.VolumeStepsFragment;
 
 public class SettingsGateway {
 
@@ -288,7 +289,8 @@ public class SettingsGateway {
             GlobalActionsPanelSettings.class.getName(),
             DarkModeSettingsFragment.class.getName(),
             BenzoSettings.class.getName(),
-            GlobalActionsSettings.class.getName()
+            GlobalActionsSettings.class.getName(),
+            VolumeStepsFragment.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -329,5 +331,6 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             Settings.BenzoSettingsActivity.class.getName(),
             Settings.GlobalActionsSettingsActivity.class.getName(),
+            Settings.VolumeStepsFragmentActivity.class.getName(),
     };
 }

AospExtended/platform_packages_apps_Settings

Signed-off-by: Jason Edson <jaysonedson@gmail.com>
Signed-off-by: DennySPb <dennyspb@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -164,6 +164,7 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import org.aospextended.extensions.Extensions;
 import org.aospextended.extensions.fragments.GamingMode;
+import org.aospextended.extensions.fragments.VolumeStepsFragment;
 
 import com.android.settings.livedisplay.LiveDisplaySettings;
 
@@ -320,6 +321,7 @@ public class SettingsGateway {
             Extensions.class.getName(),
             RunningServices.class.getName(),
             GamingMode.class.getName(),
+            VolumeStepsFragment.class.getName(),
 
             // LiveDisplay
             LiveDisplaySettings.class.getName()
@@ -365,6 +367,7 @@ public class SettingsGateway {
             // AEX additions
             Settings.ExtensionsSettingsActivity.class.getName(),
             Settings.DevRunningServicesActivity.class.getName(),
-            Settings.GamingModeActivity.class.getName()
+            Settings.GamingModeActivity.class.getName(),
+            Settings.VolumeStepsFragmentActivity.class.getName()
     };
 }