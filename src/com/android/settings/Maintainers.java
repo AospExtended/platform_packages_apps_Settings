667fc87a0b361108768f2ea5fede4f4b74d82399
Shubham Bhise <shubhamprince111@gmail.com>
2022-02-15 17:54:42 +0100

    AboutUs: Update api url to s
    Change-Id: Ife68b77ad4ce6ac731415a5f3171564f29801115
    Signed-off-by: Shubham Bhise <shubhamprince111@gmail.com>

--- src/com/android/settings/Maintainers.java
+++ src/com/android/settings/Maintainers.java
@@ -60,7 +60,7 @@ public class Maintainers extends SettingsPreferenceFragment {
 
     private final String TAG = this.getClass().getSimpleName();
     private static final String REQUEST_TAG = "loadDeviceList";
-    private static final String URL = "https://aospextended.com/api/getDevices/pie/";
+    private static final String URL = "https://aospextended.com/api/getDevices/s/";
     private static final String DOWNLOAD_WEBSITE = "https://downloads.aospextended.com";
     private static final String SHARED_PREF_NAME = "extended";
     private static final int MENU_RELOAD  = 0;
